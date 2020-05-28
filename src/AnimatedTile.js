25347c7295622373543cd77083887a6102cf38e1
Noah Bennett <nbennett.320@gmail.com>
2020-05-27 23:55:10 -0400

    init

---
+++ src/AnimatedTile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class AnimatedTile extends Component {
+  render() {
+    const { 
+      image, 
+       
+    }
+    <div >
+
+    </div>
+  }
+}
